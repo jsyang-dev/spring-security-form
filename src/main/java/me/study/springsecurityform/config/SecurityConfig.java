82a435c1d0a0534db2b6483b2da8da051ce6f3c9
Jeongsu Yang <mycat83@gmail.com>
2019-09-20 16:22:06 +0900

    스프링 시큐리티 커스터마이징: JPA 연동

--- src/main/java/me/study/springsecurityform/config/SecurityConfig.java
+++ src/main/java/me/study/springsecurityform/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package me.study.springsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,18 +11,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/", "/info").permitAll()
+                .mvcMatchers("/", "/info", "/account/**").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated();
         http.formLogin();
         http.httpBasic();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("yangjs").password("{noop}1234").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}!@#$").roles("ADMIN");
     }
 }
